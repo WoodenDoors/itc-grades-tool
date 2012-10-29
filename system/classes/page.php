38f28ec3a7ec73d2454c4db2052e1eab379a66b2
Marius Rüter <mm.rueter@gmail.com>
2012-10-29 14:23:41 +0100

    Klasse Page angelegt

---
+++ system/classes/page.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Die Klasse page handhabt das ausgeben und modifizieren einer Seite
+ *
+ * @author Marius Rüter
+ */
+class page {
+    //put your code here
+}
+
+?>

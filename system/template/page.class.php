e222af03efe83f5293322b34c5719ac3a4135d4f
Marius Rüter <mm.rueter@gmail.com>
2012-12-16 21:39:33 +0100

    Seitennavigation um Beschreibungen erweitert. Vorbereitung für Startseite als Hub

--- system/template/page.class.php
+++ system/template/page.class.php
@@ -63,39 +63,10 @@ class page {
             $nav_entry->fillin("ACTIVE", '');
             $nav_entry->fillin("NAVID", $navelement->id);
             $nav_entry->fillin("NAVURL", $navelement->url);
-            $nav_entry->fillin("NAVTITLE", $navelement->title);
+            $nav_entry->fillin("NAVTITLE", " ".$navelement->title);
+            $nav_entry->fillin("ICON", $navelement->icon);
 
         }
-/*            $nav_entry->readin("style/".$this->style."/tpl/tpl_nav_entry.html");
-            $nav_entry->fillin("ACTIVE", ' class="active"');
-            $nav_entry->fillin("NAVID", "link-home");
-            $nav_entry->fillin("NAVURL", "index.php");
-            $nav_entry->fillin("NAVTITLE", "Startseite");
-            
-            $nav_entry->readin("style/".$this->style."/tpl/tpl_nav_entry.html");
-            $nav_entry->fillin("ACTIVE", '');
-            $nav_entry->fillin("NAVID", "");
-            $nav_entry->fillin("NAVURL", "viewGrades.php");
-            $nav_entry->fillin("NAVTITLE", "Notenübersicht");
-            
-            $nav_entry->readin("style/".$this->style."/tpl/tpl_nav_entry.html");
-            $nav_entry->fillin("ACTIVE", '');
-            $nav_entry->fillin("NAVID", "");
-            $nav_entry->fillin("NAVURL", "addGrades.php");
-            $nav_entry->fillin("NAVTITLE", "Noten eintragen");
-            
-            $nav_entry->readin("style/".$this->style."/tpl/tpl_nav_entry.html");
-            $nav_entry->fillin("ACTIVE", '');
-            $nav_entry->fillin("NAVID", "");
-            $nav_entry->fillin("NAVURL", "#");
-            $nav_entry->fillin("NAVTITLE", "Grafen");
-            
-            $nav_entry->readin("style/".$this->style."/tpl/tpl_nav_entry.html");
-            $nav_entry->fillin("ACTIVE", '');
-            $nav_entry->fillin("NAVID", "");
-            $nav_entry->fillin("NAVURL", "about.php");
-            $nav_entry->fillin("NAVTITLE", "Über das Projekt");*/
-
         $this->template->fillin("NAVIGATION", $nav_entry->get_template());
         
         $this->template->fillin("FOOTER", "Ein GDI2 Projekt");

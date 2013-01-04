eeacd55266ba5337837245cc8e44d09cbe4c87d8
Sebi <sebastian-zier@web.de>
2013-01-04 15:47:47 +0100

    addGrades macht jetzt was

--- system/handlers/addGradesHandler.class.php
+++ system/handlers/addGradesHandler.class.php
@@ -7,26 +7,33 @@
 require_once('pageHandler.class.php');
 
 class addGradesHandler extends pageHandler {
-    private $subject;
+    private $course;
     private $grade;
     function __construct() {
         parent::__construct();
     }
-    function submitGrades(){
-        if (checkIfLogin()){
-            insertRows('itc-grades-tool_grades', $subject, $grade);
-            return false;            
-        }
+   // function submitGrades(){
+        
+   //     $this->db->insertRows('itc-grades-tool_grades', $course, $grade);
+   //     return false;            
+   // }
+    function getCourseID($pCourse){
+        $query = $this->db->selectRows('itc-grades-tool_courses','CourseID','Abbrevation',$pCourse);
+        return $this->db->fetchAssoc($query);
     }
-    function validateGrades(){
-        /*
-         * $subjekt und $grade werden 
-         * aus dem form gefüllt
-         */
-        if(($grade<1.0)||($grade>6.0)){
-            return false;
+    
+    function getUserID($pUser){
+        $query = $this->db->selectRows('itc-grades-tool_users','ID','username',$pUser);
+        return $this->db->fetchAssoc($query);
+    }
+    function validateGrades($pUserID,$pGrade,$pCourse){
+        
+        if(($pGrade<1.0)||($pGrade>6.0)){
+            return true;
         }
-        submitGrades();
+        //$this->submitGrades();
+        $this->db->insertRow('itc-grades-tool_grades', [$pUserID, $pGrade, $pCourse],"UserID, grade, CourseID");
+        return false;
     }
 }
 ?>
--- webroot/addGrades.php
+++ webroot/addGrades.php
@@ -18,18 +18,26 @@ if (!$login) {
     $vorname = $handler->getVorname();
     $nachname = $handler->getNachname();
     $email = $handler->getEmail();
+    $UserID = $handler->getUserID($username);
     
     $content.=
     '<form action="'.$_SERVER['REQUEST_URI'].'" method="post" enctype="multipart/form-data" accept-charset="UTF-8">
-        <label for="course">Fach:</label>
-        <select name="course" id="course">
-            <option value="MCI">MCI</option>
-            <option value="Prog1">Programmierung 1</option>
-        </select><br/>
-        <label for="grade" id="grade">Note:</label>
-        <input name="grade" id="grade" value="1.0"/><br/>
-        <input type="submit" value="Absenden"/>
+         <fieldset id="hinzufuegen">   
+            <label for="course">Fach:</label>
+            <select name="course" id="course">
+                <option value="MCI">MCI</option>
+                <option value="Prog1">Programmierung 1</option>
+            </select><br/>
+            <label for="grade">Note:</label>
+            <input name="grade" id="grade" value="1.0"/><br/>
+            <button class="button fancyBtn" id="name" name="submit">Absenden</button>
+         </fieldset>
     </form>';
+    if(isset($_POST['submit'])) {
+        $course = $_POST['course'];
+        $course = $handler->getCourseID($course);
+        $handler->validateGrades($UserID['ID'],$_POST['grade'], $course['CourseID']);
+    }
 }
 $page->set_body_content($content);
 echo $page->get_page();

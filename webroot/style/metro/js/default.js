4f09dc5b387b3c6f95edec13d00720a52fb6dd88
Marius Rüter <mm.rueter@gmail.com>
2013-01-09 13:11:39 +0100

    Metro Charms Modal fix

---
+++ webroot/style/metro/js/default.js
@@ -0,0 +1,50 @@
+/*
+ Created on : 05.11.12, 19:49
+ Author     : mwegmann
+ Description: default script
+ */
+$(document).ready(function(){
+
+    // rotator just for the heck of it for now
+    // TODO -- might have to disable (doesn't spin in FF and possibly other)
+
+    var rotator = $("div#rotator");
+    hideRotator();
+
+
+    // Pflichtfelder mit * markieren
+    $("form input[required]")
+        .after('<span class="required">*</span>');
+
+    // Stand-alone Buttons
+    $("button#settings").click(function(){
+        window.location.href = "settings.php";
+        showRotator();
+    });
+
+
+    $("button#logout").click(function(){
+        window.location.href = "logout.php";
+        showRotator();
+    });
+
+    $("button#login").click(function(){
+        window.location.href = "login.php";
+        showRotator();
+    });
+
+    $("button#register").click(function(){
+        window.location.href = "register.php";
+        showRotator();
+    });
+
+
+    function showRotator(){
+        rotator.css({visibility: "visible", opacity: "1", display: "block"});
+    }
+
+    function hideRotator(){
+        rotator.fadeTo("fast", 0);
+    }
+
+});

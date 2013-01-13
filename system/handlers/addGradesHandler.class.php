8f3dd8b5d6d441c35d6b5c97e0274187380ca6cd
Mathias We <mathes111@gmail.com>
2013-01-13 19:46:37 +0100

    fixes here and there

--- system/handlers/addGradesHandler.class.php
+++ system/handlers/addGradesHandler.class.php
@@ -27,16 +27,18 @@ class addGradesHandler extends pageHandler {
     function validateGrades($pGrade, $pCourse){
         $courseID = $this->getCourseID($pCourse);
 
-        // Note zwischen 1.0 und 6.0
-        if( ($pGrade<1.0) || ($pGrade>6.0) ){
+        // Note zwischen 1.0 und 5.0
+        if( ( $pGrade<1.0 ) || ( $pGrade>5.0 ) ){
             return parent::ERR_GRADES_WRONG_SYNTAX;
         }
 
-        // TODO
         // Note darf nur bestimmte Werte
+        $validGrades = [ 1.0, 1.3, 1.7, 2, 2.3, 2.7, 3.0, 3.3, 3.7, 4.0, 5.0 ];
+        // Quellen: https://www-sec.uni-regensburg.de/pnz/index.html.de || http://www.uni-passau.de/4937.html || http://de.wikipedia.org/wiki/Schulnote#Hochschule
+        // TODO: Prüfung
 
         //Überprüfung, ob Datensatz vorhanden
-        // TODO Lieber nur eine Warnung bzw. ein "Wirklich ändern?" anzeigen
+        // TODO: Lieber nur eine Warnung bzw. ein "Wirklich ändern?" anzeigen
         $query = $this->db->selectRows(
             parent::DB_TABLE_GRADES, '*',
             ['user_id', 'course_id'],

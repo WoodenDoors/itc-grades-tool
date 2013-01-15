35e73241aa4c52b01a5d9fc1b5b94249fea8290e
Sebi <sebastian-zier@web.de>
2013-01-15 23:11:12 +0100

    layout fixed

--- webroot/viewGrades.php
+++ webroot/viewGrades.php
@@ -21,13 +21,38 @@ if (!$login){
         $content .= '<tr><th>Kürzel</th><th>Note</th></tr>';
 
         $semester=0;
+        $gradeAverageSem=0;
+        $gradeAverageAll=0;
+        $gradeNoSem=0;
+        $gradeNoAll=0;
+        $firstLoop=true;
         foreach($results as $result) {
             if( $result['semester'] > $semester ) {
+                //Für das erste Semester wird der Teil übersprungen
+                if($firstLoop){
+                    $firstLoop=false;
+                }
+                //Am Ende jedes Semesters wird der Durschnitt ausgegeben und zurückgesetzt
+                else{
+                    $gradeAverageSem = $gradeAverageSem/$gradeNoSem;
+                    $content .= "<tr><td>Avg:</td><td>".round($gradeAverageSem,2)."</td></tr>";
+                    $gradeAverageSem =0;
+                    $gradeNoSem=0;
+                }
                 $semester++;
                 $content .= "<tr><td>$semester. Semester</td></tr>";
             }
+            $gradeAverageSem += $result['grade'];
+            $gradeAverageAll += $result['grade'];
             $content .= "<tr><td>".$result['abbreviation']."<td><td>".$result['grade']."</td></tr>";
+            $gradeNoSem++;
+            $gradeNoAll++;
         }
+        //Für das letzte Semester wird nach der Schleife der Schnitt berechnet
+        $gradeAverageSem = $gradeAverageSem/$gradeNoSem;
+        $gradeAverageAll = $gradeAverageAll/$gradeNoAll;
+        $content .= "<tr><td>Avg:</td><td>".round($gradeAverageSem,2)."</td></tr>";
+        $content .= "<tr><td>Gesamt :</td><td>".round($gradeAverageAll,2)."</td></tr>";
         $content .= '</table>';
     }
 

ed74d042f501d1948a136977c26be88a8c6ca8af
Mathias We <mathes111@gmail.com>
2013-01-16 00:10:23 +0100

    switch for grades.json

--- webroot/ajax/grades.json.php
+++ webroot/ajax/grades.json.php
@@ -4,11 +4,29 @@ $handler = new viewGradesHandler();
 
 if (!$handler->checkIfLogin()) { die("Kein Zugriff."); }
 
-$set = $handler->getGrades();
-foreach($set as $item) {
-    $result[] = [
-        "Abbr" => $item['abbreviation'],
-         "Grade" => $item['grade']
-    ];
+function grades_output($handler) {
+    $set = $handler->getGrades();
+    foreach($set as $item) {
+        $result[] = [
+            "Abbr" => $item['abbreviation'],
+             "Grade" => $item['grade']
+        ];
+    }
+    return $result;
+}
+
+function credits_output($handler) {
+    // TODO ADD THIS
+    return "";
+}
+
+
+$type = (isset($_GET['type'])) ? $_GET['type'] : "grades";
+
+switch($type) {
+    case "credits":
+        $result = credits_output($handler); break;
+    default:
+        $result = grades_output($handler);
 }
 echo json_encode($result);
